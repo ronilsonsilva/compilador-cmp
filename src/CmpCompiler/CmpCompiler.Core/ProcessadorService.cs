fce353816676730a7ab209889334294ca929afd7
Sousa Silva Engenharia de Sistemas <rs.ronilsonsilva@outlook.com.br>
2021-03-02 19:14:48 -0300

    Processamento de arquivo a partir de um path específico

---
+++ src/CmpCompiler/CmpCompiler.Core/ProcessadorService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CmpCompiler.Core
+{
+    public class ProcessadorService
+    {
+        #region [Constructors]
+
+        public ProcessadorService() { }
+
+        #endregion
+
+        #region [Methods]
+
+        public void FromFile(string path)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(path);
+                List<string> sb = new List<string>();
+                foreach (var line in lines)
+                {
+                    if (line.Contains("\\\\", StringComparison.CurrentCulture))
+                    {
+                        var lineSplit = line.Split("\\\\");
+                        if(!string.IsNullOrEmpty(lineSplit[0]))
+                            sb.Add(lineSplit[0]);
+                    }
+                    else sb.Add(line);
+                }
+                string pathNovoArquivo = Path.ChangeExtension(path, ".proc");
+                if (File.Exists(pathNovoArquivo))
+                    File.Delete(pathNovoArquivo);
+                File.WriteAllLines(pathNovoArquivo, sb);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/CmpCompiler/CmpCompiler/Program.cs
@@ -0,0 +1,19 @@
+﻿using CmpCompiler.Core;
+using System;
+
+namespace CmpCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o path do aquivo, com extensão .cmp");
+
+            var path = Console.ReadLine();
+
+            new ProcessadorService().FromFile(path);
+
+            Console.WriteLine("Processamento finalizado!!!!!");
+        }
+    }
+}
